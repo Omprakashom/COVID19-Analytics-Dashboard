2bcbea46a9ad5798e23881aebd9c1d654389ed05
Omprakashom <154739002+Omprakashom@users.noreply.github.com>
2026-07-16 10:36:58 +0530

    Refactor COVID-19 dashboard code structure

--- dashboard/app.py
+++ dashboard/app.py
@@ -137,7 +137,7 @@ fig.update_layout(
     yaxis_title="New Deaths"
 )
 
-st.plotly_chart(fig, use_container_width=True)
+st.plotly_chart(fig, width="stretch")
 
 # ---------------------------------------
 # Vaccination Chart
